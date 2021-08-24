fc83dc4e74fbf218657842e284dd5a28fd752861
DongYinhang <13023695951@163.com>
2021-08-24 17:08:05 +0800

    edit

--- src/test/java/com/honey/bank/backstage/BackstageApplicationTests.java
+++ src/test/java/com/honey/bank/backstage/BackstageApplicationTests.java
@@ -1,13 +1,23 @@
 package com.honey.bank.backstage;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class BackstageApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        System.err.println(connection.getClass());
     }
 
 }
